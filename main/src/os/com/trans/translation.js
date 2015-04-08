c5ae954baef64b5db3d43ed8c154a931260f0cde
xincai juan <jxcjxcjzx@sina.com>
2015-04-09 01:01:46 +0800

    simple log and parser framework add
    add a simple lisp2js parser and a logcat tool for debugging

---
+++ main/src/os/com/trans/translation.js
@@ -0,0 +1,61 @@
+// in this tiny file 
+// a micro lisp-2-js translating 
+//  tool is created, just for testing , 
+// or something called DSL(Dommain-Specified-Language)
+
+// by jxc 2015/04/08
+
+exports = {
+	
+	function lisp2js(lisp_program_fragment){
+		
+		// so some simple calculating here
+		// and return the result
+		
+		
+	}
+
+
+}
+
+
+// bunches of functions to parse and calculating 
+// the tasks specified by lisp programs 
+function tokenize(){
+
+
+}
+
+function parse(){
+
+}
+
+
+
+
+// may have a spelling error of word "atome"
+functionArray atomeFunction[] = {
+	// some basic functions in js to 
+	// form the basic of calculating
+	add:{}
+	
+	minus:{}
+	
+	print:{}
+	
+	eval:{}
+	
+	// ect .... can be added some other functions
+
+}
+
+
+
+function calculating(){
+	
+
+}
+
+
+
+
---
+++ main/tools/log/LogCat.js
@@ -0,0 +1,37 @@
+
+// this is a log recording tool for debug 
+// purpose only .
+
+exports = {
+
+	function : beginRecord(String name){
+	
+	}
+	
+	
+	function : trace(){
+		// print the key process information 
+		// and the system time for debug
+		
+	
+	}
+
+}
+
+function printLog(String name){
+
+
+}
+
+function dumpProcess(){
+	// ps : tips are the same to
+	// that of function getSystemTime
+	
+}
+
+function getSystemTime(){
+	// ps : this function can be removed to 
+	// the tools lib in the future
+
+}
+
