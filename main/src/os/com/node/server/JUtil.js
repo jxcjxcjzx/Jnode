9a944c993c7b84f4ae70c3bd9692d80d5024f85a
xincai juan <jxcjxcjzx@sina.com>
2015-06-03 23:15:43 +0800

    normal  update
    add some libs for system server functioncality

---
+++ main/src/os/com/node/server/JUtil.js
@@ -0,0 +1,129 @@
+
+ var managers = {
+	
+	ConfigurationManager a;
+	
+ 
+ }
+ 
+ var sql_command_template = {
+	add :
+	update :
+	del :
+	select :
+	
+ }
+ 
+ 
+ var sql = {
+ 
+	SqlDataAdapter adapter = {
+		SelectCommand cmd;
+		
+		function fill(){}
+	};
+ 
+	SqlParameters params ;
+	
+	SqlCommand sqlcmd = {
+		Parameters params = {
+			function addRange(){}
+		};
+		
+		CommandTimeout time;
+	};
+	
+	
+ }
+
+ var Consts = {
+	
+	Convert convert;
+	
+	SqlConnection sqlcon = {
+	
+		function open(){}
+		
+		
+	};
+	
+	KeyValuePair pair;
+	
+	DataSet set;
+	
+	SortedDictionery dic;
+	
+	StringBuilder builder = {
+		function appendLine(){}
+		
+		function ToString(){}
+	};
+	
+	function foreach(){}
+	
+	
+	
+	DataTable table = {
+		function selectRow(){}
+	};
+	
+	DataList list = {
+		function containsKey(){}
+	};
+	
+	DataFormat format = {
+		function DataFormat(String format(default="yyyy-MM-dd")){}
+	}
+	
+	DateTime time = {
+		Now now = {
+			function AddDays(){}
+			
+			function toString(DataFormat format){}
+		};
+	};
+	
+	DataRow row;
+	
+
+	
+ 
+ }
+ 
+ function SqlHelper(){
+	
+	ExecuteNonQuery.static = function(str,cmdType){}
+	
+	ExecuteData
+ 
+ }
+ 
+ function checkVid(){
+ 
+ }
+ 
+ function getDataStatus(){
+ 
+ }
+ 
+ function getCount(){
+ 
+ }
+ 
+ function getFilePath(){
+ 
+ }
+ 
+ function checkUser(){
+ 
+ }
+ 
+ function getBaseInfoTable(){
+ 
+ }
+ 
+ 
+ 
+ 
+ 
+ 
---
+++ main/src/os/com/node/server/server.js
@@ -0,0 +1,4 @@
+
+ var consts = {
+	UserInfo info;
+ }
